5b6657fe91a2b58c298b427e4b1a6efbbecfabc4
Bárbara Melo <ibarbaramelo@gmail.com>
2026-04-23 10:51:24 -0300

    Subindo o app

---
+++ app.py
@@ -0,0 +1,10 @@
+print ('NHACAPP\n')
+
+print ('1. Cadastrar Restaurante')
+print ('2. Listar Restaurante')
+print ('3. Ativar Restaurante')
+print ('4. Sair\n')
+
+
+opcao_escolhida = input('Escolha uma opção: ')
+print ('Você escolheu a opção', opcao_escolhida)
